b43c9efa15bdec98a92ceed1634c3ee08394bf0e
Biggie <biggie@Biggies-MacBook-Pro.local>
2021-01-16 01:46:09 +0100

    2nd commit

--- twosum.js
+++ twosum.js
@@ -10,7 +10,7 @@ const twoSum = (nums, target) => {
         //iterate the given array [2,7,11,15] = nums
     
     for (let [index, num] of nums.entries()) {
-        //now we extracting the index and actual number from the number of              entries
+        //now we extracting the index and actual number from the number of entries
         //next thing is to check if out index value is not undefined
         if(storage[num] !== undefined)
             return [storage [num],index];
@@ -18,3 +18,22 @@ const twoSum = (nums, target) => {
         storage[target-num] = index;
     } 
 };
+
+/* [2,7,11,15]
+on going through the loop the first time
+value/num = 2
+index = 0
+storage at num 2 doesnt exist cos its an empty object
+now taking the target and subtracting the num from it (9-2)
+
+storage {
+ 7 ; key
+ 0 ; index {now i have the index of where that particular element would be}
+}
+
+looping again for the second time 
+value/num = 7
+index = 1
+
+
+*/ 
